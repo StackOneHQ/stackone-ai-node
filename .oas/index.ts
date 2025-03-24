a494595f31054329481bedc96882c75398d2673a
Matt Carey <matt@stackone.com>
2025-03-24 11:03:43 -0700

    move to ts files for imports

---
+++ .oas/index.ts
@@ -0,0 +1,7 @@
+export { coreSpec } from './core';
+export { crmSpec } from './crm';
+export { atsSpec } from './ats';
+export { hrisSpec } from './hris';
+export { documentsSpec } from './documents';
+export { marketingSpec } from './marketing';
+export { ticketingSpec } from './ticketing';
---
+++ .oas/ticketing.ts
@@ -0,0 +1,35 @@
+// Generated OpenAPI specification for ticketing
+export const ticketingSpec = {
+  "openapi": "3.1.0",
+  "paths": {},
+  "info": {
+    "title": "Ticketing",
+    "description": "The documentation for the StackOne Unified API - TICKETING",
+    "version": "1.0.0",
+    "contact": {}
+  },
+  "tags": [
+    {
+      "name": "Tickets",
+      "description": ""
+    },
+    {
+      "name": "Users",
+      "description": ""
+    }
+  ],
+  "servers": [
+    {
+      "url": "https://api.stackone.com"
+    }
+  ],
+  "components": {
+    "securitySchemes": {
+      "basic": {
+        "type": "http",
+        "scheme": "basic"
+      }
+    },
+    "schemas": {}
+  }
+} as const;
