d1d0691b61f41337f01113ba0bdcc3150ccfc6e3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-06-23 16:51:07 +0100

    chore: format tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,23 +1,25 @@
 import { defineConfig } from 'tsdown';
+import { $ } from 'bun';
 
 export default defineConfig({
-	entry: [ "./src/index.ts" ],
-	outDir: 'dist',
-	format: 'esm',
-	clean: true,
-	sourcemap: true,
-	treeshake: true,
-	dts: {
+  entry: ['./src/index.ts'],
+  outDir: 'dist',
+  format: 'esm',
+  clean: true,
+  sourcemap: true,
+  treeshake: true,
+  dts: {
     tsgo: true,
-    resolve: [/^@types\//]
+    resolve: [/^@types\//],
   },
-	publint: true,
-	unused: true,
-	unbundle: true,
-	exports: true,
- });
-  hooks:{
+  publint: true,
+  unused: true,
+  unbundle: true,
+  exports: true,
+  hooks: {
     'build:done': async () => {
       // sourcemap files for generated code are not needed
-      await $`rm -rf ./dist/openapi/generated/*.map`
-    }
+      await $`rm -rf ./dist/openapi/generated/*.map`;
+    },
+  },
+});
