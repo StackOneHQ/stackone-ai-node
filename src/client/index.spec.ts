183425166a5ede4949d1150538eb4271f5769a16
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-12-09 10:13:29 +0000

    refactor(client): flatten directory structure
    - Merge rpc-client.ts implementation into client/index.ts
    - Rename tests/rpc-client.spec.ts to index.spec.ts
    - Remove empty client/tests/ directory
    - Update import paths in test file

--- src/client/index.spec.ts
+++ src/client/index.spec.ts
@@ -1,5 +1,5 @@
-import { StackOneAPIError } from '../../utils/errors';
-import { RpcClient } from '../rpc-client';
+import { StackOneAPIError } from '../utils/errors';
+import { RpcClient } from './index';
 
 describe('RpcClient', () => {
   describe('constructor', () => {
